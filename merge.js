5830893753a8b23e9860da17aee87754c0442ce9
nanthil <macks.smart@hotmail.com>
2017-11-19 16:26:23 -0700

    modified naming for mergesort

--- merge.js
+++ merge.js
@@ -16,7 +16,7 @@ function mergeSortIterator(data){
     let count = 0
     this.merge = function(r,sign,sorted = []) {
         while(this.hasAdditionalElements()|| r.hasAdditionalElements())
-            sorted.push(largest(this,r,sign))
+            sorted.push(smallestOrLargest(this,r,sign))
         return sorted
     }
     this.next = function(){
@@ -33,12 +33,12 @@ function mergeSortIterator(data){
     return this
 }
 
-const largest = (iterA,iterB,lt_or_gt) => 
-    (isNaN(iterA.current() - iterB.current()))// either ma or mb current value is undefined
-    ? (iterA.next() || iterB.next())          // push only the defined value
-    : eval('(iterA.current() '+ lt_or_gt +'= iterB.current())') // otherwise use the sign to determind which of the two is greater than/lessthan
-    ? (iterA.next()) //push a if a gt/lt b
-    : (iterB.next()) // push b if b gt/lt a
+const smallestOrLargest = (left,right,lt_or_gt) => 
+    (isNaN(left.current() - right.current()))// either ma or mb current value is undefined
+    ? (left.next() || right.next())          // push only the defined value
+    : eval('(left.current() '+ lt_or_gt +'= right.current())') // otherwise use the sign to determind which of the two is greater than/lessthan
+    ? (left.next()) //push a if a gt/lt b
+    : (right.next()) // push b if b gt/lt a
 
 const split = (a, len = Math.floor(a.length / 2)) => [a.slice(0,len), a.slice(len, a.length)]
 
