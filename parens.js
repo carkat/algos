b68382bf6ccf7807f78cc75a91421dece920d198
nanthil <macks.smart@hotmail.com>
2017-11-19 16:29:34 -0700

    modified parens for legibility

--- parens.js
+++ parens.js
@@ -1,10 +1,15 @@
-const test = (str, //accepts one param
-    stack = [], unMatchedClosingBracket = false, open = '({[', close = ')}]',
-    evaluateEachCharacter = str.split('').forEach(
-        x => open.includes(x) ? stack.push(x) 
-            : close.includes(x) && (close.indexOf(x) === open.indexOf(stack.slice(-1))) ? stack.pop()
-            : unMatchedClosingBracket = true)
-) => !unMatchedClosingBracket && stack.length === 0
+const test = (str,stack = [], unMatchedClosingBracket = false) =>{
+    const open = '({[', close = ')}]'
+    const evaluateEachCharacter = str.split('').forEach(x => open.includes(x) 
+            ? stack.push(x) 
+            : close.includes(x) && (close.indexOf(x) === open.indexOf(stack.slice(-1))) 
+            ? stack.pop()
+            : unMatchedClosingBracket = true) 
+    return  !unMatchedClosingBracket && stack.length === 0
+}
+//one liner for the lulz
+//const test = (str,stack = [], unMatchedClosingBracket = false, open = '({[', close = ')}]',evaluateEachCharacter = str.split('').forEach(x => open.includes(x) ? stack.push(x) : close.includes(x) && (close.indexOf(x) === open.indexOf(stack.slice(-1))) ? stack.pop(): unMatchedClosingBracket = true)) => !unMatchedClosingBracket && stack.length === 0
+
 
 console.log('testA is: ', test('{}{(())[[]][({})]()}'))
 console.log('testB is: ', test('{}{(())[[]][({})]()(}'))
